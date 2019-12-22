433c9a3e6000c1fafd414bdfddab83da95ccc8cd
Kelly Widmann <kawidmann90@gmail.com>
2019-12-22 11:56:02 -0500

    Authenticate plug now uses Accounts context instead of a direct call to Repo

--- lib/myflix_web/plugs/authenticate.ex
+++ lib/myflix_web/plugs/authenticate.ex
@@ -2,8 +2,7 @@ defmodule MyflixWeb.Plugs.Authenticate do
   import Plug.Conn
   import Phoenix.Controller, only: [redirect: 2]
 
-  alias Myflix.Repo
-  alias Myflix.Accounts.User
+  alias Myflix.Accounts
 
 
   def init(_params) do
@@ -13,7 +12,7 @@ defmodule MyflixWeb.Plugs.Authenticate do
     user_id = Plug.Conn.get_session(conn, :current_user_id)
 
     cond do
-      current_user = user_id && Repo.get(User, user_id) ->
+      current_user = user_id && Accounts.get_user!(user_id) ->
         conn
         |> assign(:current_user, current_user)
         |> assign(:user_signed_in?, true)
